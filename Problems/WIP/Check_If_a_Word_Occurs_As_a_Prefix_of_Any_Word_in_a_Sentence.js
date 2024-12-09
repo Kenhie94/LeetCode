3644cba8fb7991bf469cca450ec84a2ba75689df
Kenjy Jap <kenhiebusiness@gmail.com>
2024-12-09 11:23:19 -0800

    Solved question 1455

--- Problems/WIP/Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.js
+++ Problems/WIP/Check_If_a_Word_Occurs_As_a_Prefix_of_Any_Word_in_a_Sentence.js
@@ -4,24 +4,20 @@
 
 // A prefix of a string s is any leading contiguous substring of s.
 
-
-
 // Example 1:
-
 // Input: sentence = "i love eating burger", searchWord = "burg"
 // Output: 4
 // Explanation: "burg" is prefix of "burger" which is the 4th word in the sentence.
-// Example 2:
 
+// Example 2:
 // Input: sentence = "this problem is an easy problem", searchWord = "pro"
 // Output: 2
 // Explanation: "pro" is prefix of "problem" which is the 2nd and the 6th word in the sentence, but we return 2 as it's the minimal index.
-// Example 3:
 
+// Example 3:
 // Input: sentence = "i am tired", searchWord = "you"
 // Output: -1
 // Explanation: "you" is not a prefix of any word in the sentence.
-
 
 // Constraints:
 
@@ -36,5 +32,11 @@
  * @return {number}
  */
 var isPrefixOfWord = function(sentence, searchWord) {
-    
+  let newSentence = sentence.split(' ')
+  for (let i = 0; i < newSentence.length; i++) {
+      if (newSentence[i].startsWith(searchWord)) {
+          return i + 1
+      }
+  }
+  return -1
 };
